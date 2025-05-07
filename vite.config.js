383cceffbb45357df2ad1fdb93bff7c6ec703edf
Han <debra4117@gmail.com>
2025-05-06 22:47:26 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+// vite.config.js
+ import react       from "@vitejs/plugin-react";
+ import path        from "path";
+ import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+ export default defineConfig({
+plugins: [ react() ],
+   resolve: {
+     alias: {
+       "@": path.resolve(__dirname, "./src"),
+     },
+   },
+ });
